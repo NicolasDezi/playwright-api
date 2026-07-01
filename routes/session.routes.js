bfeb480c7402e8485bd7ec9724e88bc165f72e54
Nicolas Dezi <nicolas.dezi@gmail.com>
2026-07-01 18:10:38 -0300

    Refactor project structure

--- routes/session.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const SessionController = require("../controllers/SessionController");
-
-router.post("/create", SessionController.create);
-
-router.post("/close", SessionController.close);
-
-module.exports = router;
